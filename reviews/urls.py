9f33328e3416614794ee9243ad038613ff4c8429
Will Lowry <william.c.lowry@gmail.com>
2017-07-31 22:54:35 +0000

    review stuff

--- reviews/urls.py
+++ reviews/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     # ex: /review/user - get reviews for the logged user
     url(r'^review/user/(?P<username>\w+)/$', views.user_review_list, name='user_review_list'),
     url(r'^review/user/$', views.user_review_list, name='user_review_list'),
+
+    # ex: /recommendation - get album recommendations for the logged user
+    url(r'^recommendation/$', views.user_recommendation_list, name='user_recommendation_list'),
 ]
